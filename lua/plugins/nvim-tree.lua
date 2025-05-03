4f66fc098862786e968964956b1ffa7a9cf8165e
oliRenan <oliveirarenan479@gmail.com>
2025-05-03 07:43:01 -0300

    testando o vim fugitive

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,47 +1,46 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
   lazy = false,
   dependencies = {
-      "nvim-tree/nvim-web-devicons",
+    'nvim-tree/nvim-web-devicons',
   },
 
   config = function()
-      local nvimtree = require("nvim-tree")
-      nvimtree.setup({
-          view = {
-              width = 35,
-              relativenumber = true,
+    local nvimtree = require 'nvim-tree'
+    nvimtree.setup {
+      view = {
+        width = 35,
+        relativenumber = true,
+      },
+      -- change folder arrow icons
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = '', -- arrow when folder is closed
+              arrow_open = '', -- arrow when folder is open
+            },
           },
-          -- change folder arrow icons
-          renderer = {
-              indent_markers = {
-                  enable = true,
-              },
-              icons = {
-                  glyphs = {
-                      folder = {
-                          arrow_closed = "", -- arrow when folder is closed
-                          arrow_open = "", -- arrow when folder is open
-                      },
-                  },
-              },
+        },
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
           },
-          -- disable window_picker for
-          -- explorer to work well with
-          -- window splits
-          actions = {
-              open_file = {
-                  window_picker = {
-                      enable = false,
-                  },
-              },
-          },
+        },
+      },
 
-          git = {
-              ignore = false,
-          },
-      }
-      )
-  end
+      git = {
+        ignore = false,
+      },
+    }
+  end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,68 @@
-return { -- Highlight, edit, and navigate code
+-- return { -- Highlight, edit, and navigate code
+--   'nvim-treesitter/nvim-treesitter',
+--   build = ':TSUpdate',
+--   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+--   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+--   opts = {
+--     ensure_installed = {
+--       'lua',
+--       'python',
+--       'javascript',
+--       'typescript',
+--       'vimdoc',
+--       'vim',
+--       'regex',
+--       'terraform',
+--       'sql',
+--       'dockerfile',
+--       'toml',
+--       'json',
+--       'java',
+--       'groovy',
+--       'go',
+--       'gitignore',
+--       'graphql',
+--       'yaml',
+--       'make',
+--       'cmake',
+--       'markdown',
+--       'markdown_inline',
+--       'bash',
+--       'tsx',
+--       'css',
+--       'html',
+--     },
+--     -- Autoinstall languages that are not installed
+--     auto_install = true,
+--     highlight = {
+--       enable = true,
+--       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+--       --  If you are experiencing weird indenting issues, add the language to
+--       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+--       additional_vim_regex_highlighting = { 'ruby' },
+--     },
+--     indent = { enable = true, disable = { 'ruby' } },
+--   },
+--   incremental_selection = {
+--     enable = true,
+--     keymaps = {
+--         init_selection = "<C-space>",
+--         node_incremental = "<C-space>",
+--         scope_incremental = false,
+--         node_decremental = "<bs>",
+--     },
+-- },
+--
+-- }
+--
+--
+--
+--
+
+return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+  main = 'nvim-treesitter.configs',
   opts = {
     ensure_installed = {
       'lua',
@@ -32,25 +92,23 @@ return { -- Highlight, edit, and navigate code
       'css',
       'html',
     },
-    -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
       enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { 'ruby' },
     },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
+    indent = {
+      enable = true,
+      disable = { 'ruby' },
     },
-},
-
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<C-space>',
+        node_incremental = '<C-space>',
+        scope_incremental = false,
+        node_decremental = '<bs>',
+      },
+    },
+  },
 }
