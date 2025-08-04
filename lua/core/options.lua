143436142f578774ceb5ec9ceffc9ae1efa010d8
oliRenan <oliveirarenan479@gmail.com>
2025-08-04 17:13:35 -0300

    ultimo commit antesde de usar o i3

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor = ''
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -14,7 +14,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -23,10 +23,13 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append '@-@'
 
 vim.opt.updatetime = 50
 vim.opt.clipboard = 'unnamedplus'
 
---:vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = '80'
+vim.cmd 'highlight ColorColumn ctermbg=0 guibg=#2c2c2c'
+
+vim.opt.textwidth = 180
