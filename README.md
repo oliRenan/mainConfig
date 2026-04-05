d98d92b8d699d30e1cc5a9acad00c37d017e09bb
oliRenan <oliveirarenan479@gmail.com>
2026-04-05 16:47:51 -0300

    change deprecated configs

--- README.md
+++ README.md
@@ -2,3 +2,100 @@
 
 This repo contains my custom Neovim config for 2025, built on the foundation of [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim). This setup is designed to enhance your Neovim experience with modern features and seamless integration.
 Config inspriara no [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim)
+
+# Guia Definitivo: Neovim + Tree-sitter no Arch Linux
+
+Este documento contém todos os comandos de terminal e o código Lua exato para instalar e configurar o Tree-sitter, resolvendo os erros de `libclang` e de API.
+
+## 1. Instalação das Dependências do Sistema
+
+O Tree-sitter e o Cargo precisam de compiladores (C/C++ e Rust) para rodar e construir os parsers. Rode no terminal:
+
+```bash
+# Atualiza os repositórios (evita erros 404) e instala os compiladores e o Rust
+sudo pacman -Syu
+sudo pacman -S base-devel clang gcc make git rust
+```
+
+## 2. Instalação do Tree-sitter CLI
+
+Instale a ferramenta de linha de comando usando o Cargo:
+
+```bash
+cargo install tree-sitter-cli --locked
+```
+
+## 3. Exportação da Variável (PATH)
+
+Para o sistema encontrar o comando `tree-sitter` recém-instalado, o diretório do Cargo precisa estar no seu PATH.
+
+Rode os comandos abaixo diretamente no seu terminal (escolha de acordo com o seu shell):
+
+**Para Zsh:**
+
+```bash
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.zshrc
+source ~/.zshrc
+```
+
+**Para Bash:**
+
+```bash
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+```
+
+## 4. Configuração do Neovim (`treesitter.lua`)
+
+Substitua todo o conteúdo do seu arquivo de configuração (geralmente em `~/.config/nvim/lua/plugins/treesitter.lua`) por este código:
+
+```lua
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      -- API correta para o setup
+      local configs = require('nvim-treesitter.configs')
+
+      configs.setup({
+        -- Parsers que serão instalados automaticamente
+        ensure_installed = {
+          'c', 'lua', 'vim', 'vimdoc', 'query', 'rust', 'ron',
+          'javascript', 'typescript', 'tsx', 'html', 'css',
+          'json', 'yaml', 'bash', 'python', 'go', 'dockerfile',
+          'markdown', 'markdown_inline', 'java', 'svelte', 'graphql'
+        },
+
+        sync_install = false,
+        auto_install = true,
+
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+
+        indent = {
+          enable = true,
+          disable = { 'yaml', 'markdown' },
+        },
+      })
+    end,
+  },
+
+  -- Auto Close Tags (JS/TS/HTML)
+  {
+    'windwp/nvim-ts-autotag',
+    ft = { 'html', 'xml', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte' },
+    config = function()
+      require('nvim-ts-autotag').setup({
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false,
+        },
+      })
+    end,
+  },
+}
+```
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,114 +1,78 @@
--- return { -- Highlight, edit, and navigate code
---   'nvim-treesitter/nvim-treesitter',
---   build = ':TSUpdate',
---   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
---   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
---   opts = {
---     ensure_installed = {
---       'lua',
---       'python',
---       'javascript',
---       'typescript',
---       'vimdoc',
---       'vim',
---       'regex',
---       'terraform',
---       'sql',
---       'dockerfile',
---       'toml',
---       'json',
---       'java',
---       'groovy',
---       'go',
---       'gitignore',
---       'graphql',
---       'yaml',
---       'make',
---       'cmake',
---       'markdown',
---       'markdown_inline',
---       'bash',
---       'tsx',
---       'css',
---       'html',
---     },
---     -- Autoinstall languages that are not installed
---     auto_install = true,
---     highlight = {
---       enable = true,
---       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
---       --  If you are experiencing weird indenting issues, add the language to
---       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
---       additional_vim_regex_highlighting = { 'ruby' },
---     },
---     indent = { enable = true, disable = { 'ruby' } },
---   },
---   incremental_selection = {
---     enable = true,
---     keymaps = {
---         init_selection = "<C-space>",
---         node_incremental = "<C-space>",
---         scope_incremental = false,
---         node_decremental = "<bs>",
---     },
--- },
---
--- }
---
---
---
---
-
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs',
-  opts = {
-    ensure_installed = {
-      'lua',
-      'python',
-      'javascript',
-      'typescript',
-      'vimdoc',
-      'vim',
-      'regex',
-      'terraform',
-      'sql',
-      'dockerfile',
-      'toml',
-      'json',
-      'java',
-      'groovy',
-      'go',
-      'gitignore',
-      'graphql',
-      'yaml',
-      'make',
-      'cmake',
-      'markdown',
-      'markdown_inline',
-      'bash',
-      'tsx',
-      'css',
-      'html',
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = {
-      enable = true,
-      disable = { 'ruby' },
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<C-space>',
-        node_incremental = '<C-space>',
-        scope_incremental = false,
-        node_decremental = '<bs>',
-      },
-    },
+  -- NOTE: treesitter CLI installation needed
+  {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
+    build = ':TSUpdate',
+    config = function()
+      -- O segredo está em usar o sub-módulo .configs
+      local configs = require 'nvim-treesitter.configs'
+
+      configs.setup {
+        -- Substitui o seu treesitter.install()
+        ensure_installed = {
+          'json',
+          'javascript',
+          'typescript',
+          'tsx',
+          'go',
+          'yaml',
+          'html',
+          'css',
+          'python',
+          'http',
+          'prisma',
+          'markdown',
+          'markdown_inline',
+          'svelte',
+          'graphql',
+          'bash',
+          'lua',
+          'vim',
+          'dockerfile',
+          'gitignore',
+          'query',
+          'vimdoc',
+          'c',
+          'java',
+          'rust',
+          'ron',
+        },
+
+        -- Habilita o realce (syntax highlighting) automaticamente
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+
+        -- Habilita a indentação inteligente baseada no Treesitter
+        indent = {
+          enable = true,
+          -- Se quiser desabilitar para tipos específicos, o Treesitter
+          -- gerencia melhor do que via autocmd manual
+          disable = { 'yaml', 'markdown' },
+        },
+
+        sync_install = false,
+        auto_install = true, -- Tenta instalar se o parser estiver faltando
+      }
+    end,
+  },
+
+  -- NOTE: js,ts,jsx,tsx Auto Close Tags
+  {
+    'windwp/nvim-ts-autotag',
+    -- Mantive sua configuração do autotag que parece correta
+    ft = { 'html', 'xml', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte' },
+    config = function()
+      require('nvim-ts-autotag').setup {
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false,
+        },
+      }
+    end,
   },
 }
